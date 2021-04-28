c50d1682fccbfb5da00a7d58189e313135265d1d
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-04-28 23:29:42 +0530

    Update test_file.cpp

--- test_file.cpp
+++ test_file.cpp
@@ -3,6 +3,11 @@
 
 #include "test/catch.hpp"
 
-TEST_CASE("") {
-  REQUIRE();
+TEST_CASE("Input valid") {
+  
+  int current_data[] = {3,3,5,10,12,11};
+  int data_size = current_data[]/current_data[0];
+  Boolean valid_input = validateReading(curren_data, data_size);
+  
+  REQUIRE(valid_input == true);
 }

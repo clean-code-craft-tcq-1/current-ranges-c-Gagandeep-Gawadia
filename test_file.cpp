1fc672598db596b5bb0fced81eb78eb88ff70499
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-04-29 00:42:21 +0530

    adding a failing test

--- test_file.cpp
+++ test_file.cpp
@@ -4,11 +4,17 @@
 #include "test/catch.hpp"
 #include "computation.h"
 
-TEST_CASE("Input valid") {
+TEST_CASE("Input set is valid") {
   
   int current_data[] = {3,3,5,10,12,11};
   int data_size = sizeof(current_data)/sizeof(current_data[0]);
   Bool valid_input = validateReading(current_data, data_size);
   
   REQUIRE(valid_input == TRUE);
+}
+
+TEST_CASE("Reading lies within the range specified") {
+  
+  Bool ReadingIsInRange = readingInRange(8, 9,7);
+  REQUIRE(ReadingIsInRange == TRUE);
 }

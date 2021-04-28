bfc279fc49f2cc78d09217df045733c3dabce2a4
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-04-28 23:41:02 +0530

    Update test_file.cpp

--- test_file.cpp
+++ test_file.cpp
@@ -2,11 +2,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "computation.c"
 
 TEST_CASE("Input valid") {
   
   int current_data[] = {3,3,5,10,12,11};
-  int data_size = current_data[]/current_data[0];
+  int data_size = sizeof(current_data)/sizeof(current_data[0]);
   Boolean valid_input = validateReading(current_data, data_size);
   
   REQUIRE(valid_input == true);
